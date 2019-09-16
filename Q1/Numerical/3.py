6ab735ac11f8829217a0f9029b0f06ec5bb787fc
Malte Wegener <malte.wegener99@gmx.de>
2019-09-16 19:19:24 +0200

    Asymetric grid

--- Q1/Numerical/3.py
+++ Q1/Numerical/3.py
@@ -8,6 +8,7 @@ def l2hnorm(x):
 err = []
 hs = []
 plot = True
+asym = True
 
 def make_A(n):
     L = np.diag((n-1)*[2],0)
@@ -15,14 +16,25 @@ def make_A(n):
     L += np.diag((n-2)*[-1],-1)
     return L
 
+def make_A_asym(n):
+    L = np.diag((n-1)*[2.0],0)
+    L += np.diag((n-2)*[-1.0],1)
+    L += np.diag((n-2)*[-1.0],-1)
+    L[0,0] = 1
+    L[0,1] = -(2/3)
+    return L
+
+
 for k in range(0,5):
     h = 0.2/(2**k)
     hs.append(h)
     n = int(1/h)
 
     omega = np.linspace(0,1, n+1)
+    if asym:
+        omega = np.delete(omega, 1)
 
-    A = make_A(n)
+    A = make_A(n) if not asym else make_A_asym(n-1)
 
     L = A/(h**2)
 
@@ -31,12 +43,14 @@ for k in range(0,5):
     fn2 = lambda x: -1*np.exp(x)+np.e*x+2
 
     wh = np.linspace(0+h, 1-h, n-1)
-    f1 = np.ones(n-1)
-    f1[0] += 1/h/h
+    if asym:
+        wh = wh[1:]
+    f1 = np.ones(wh.shape[0])
+    f1[0] += 1/h/h if not asym else 1/3/h/h
     f1[-1] += 2/h/h
 
     f2 = np.exp(wh)
-    f2[0] = 1/h/h
+    f2[0] += 1/h/h if not asym else 1/3/h/h
     f2[-1] += 2/h/h
 
     u1 = np.linalg.solve(L.copy(),f1)
@@ -49,18 +63,17 @@ for k in range(0,5):
     u2 = np.append(u2, 2)
 
     if k==0 and plot:
-        plt.plot(omega, u1, label="numerical")
+        plt.plot(omega, u1, '-x', label="numerical")
         plt.plot(omega, fn1(omega), label="analytical")
         plt.legend()
         plt.show()
 
-        plt.plot(omega, u2, label="numerical")
+        plt.plot(omega, u2, '-x', label="numerical")
         plt.plot(omega, fn2(omega), label="analytical")
         plt.legend()
         plt.show()
 
     # print(l2hnorm(u1-fn1(omega)))
-    print(np.linalg.norm(u2-fn2(omega)))
     err.append(np.linalg.norm(u2-fn2(omega))*np.sqrt(n))
 
 to_fit = lambda x,a,c: c*np.power(x,a)

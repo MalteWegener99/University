5e05a21c3f0bc6d2fc6ce3a5534fea368cf009f1
MalteWegener <malte.wegener99@gmx.de>
2019-10-09 11:06:38 +0200

    LAtex cahnges

--- Q1/Scientific/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gfortran -Wall -g -c -o fortran_array_routine.o  fortran_array_routine.f
-gcc      -Wall -g -c -o fortran_arrays.o         fortran_arrays.c 
-gcc      -Wall -g    -o fortran_arrays           fortran_arrays.o fortran_array_routine.o -lm -lgfortran
--- Q1/Scientific/tan.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python
-
-from math import pi, tan
-
-x = pi/20
-for i in range(0,11):
-    print("tan(%1.9f) = %4e \n" %(x*i, tan(x*i)))
